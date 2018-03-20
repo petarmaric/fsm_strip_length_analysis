294ab8320776a0fd87cf471ba2ebc3cd395b8904
Petar Mari? <petar.maric@gmail.com>
2018-03-20 15:43:33 +0100

    Initial shot at creating a distutils/pip compatible setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fsm_strip_length_analysis',
+    version='1.0.0',
+    url='https://bitbucket.org/petar/fsm_strip_length_analysis',
+    license='BSD',
+    author='Petar Maric',
+    author_email='petarmaric@uns.ac.rs',
+    description='Console app and Python API for strip length-dependent '\
+                'visualization and modal analysis of the parametric model of '\
+                'buckling and free vibration in prismatic shell structures, '\
+                'as computed by the fsm_eigenvalue project.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    platforms='any',
+    py_modules=['fsm_strip_length_analysis'],
+    entry_points={
+        'console_scripts': ['fsm_strip_length_analysis=fsm_strip_length_analysis:main'],
+    },
+    install_requires=open('requirements.txt').read().splitlines(),
+)
